606fa0ef74ddcf30b8f5996888fa0010e8018f9b
Ze Yen <zeyenchai@gmail.com>
2023-07-23 19:46:06 +0800

    Add login with username and password

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,9 +1,44 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: "Username", type: "text", value: "chuiyin" },
+        password: { label: "Password", type: "password", value: "asdqwe123" },
+      },
+      async authorize(credentials, req) {
+        let res = await fetch("http://localhost:5000/auth/login", {
+          method: "POST",
+          body: JSON.stringify(credentials),
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json",
+          },
+        });
+
+        const user = await res.json();
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
@@ -19,21 +54,11 @@ export const authOptions: NextAuthOptions = {
   ],
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    async jwt({ token, account }) {
-      // Persist the OAuth access_token to the token right after signin
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      return token;
+    async jwt({ token, user }) {
+      return { ...token, ...user };
     },
     async session({ session, token, user }) {
-      // Send properties to the client, like an access_token from a provider.
-      // https://stackoverflow.com/questions/74785412/nextauth-type-error-property-accesstoken-does-not-exist-on-type-session
-      session.accessToken = token.accessToken;
-
-      if (session.user) {
-        session.user.id = token.sub;
-      }
+      session.user = token as any;
 
       return session;
     },
--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -7,12 +7,10 @@ declare module "next-auth" {
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
   interface Session {
-    accessToken?: string;
     user: {
-      name?: string | null | undefined;
-      email?: string | null | undefined;
-      image?: string | null | undefined;
-      id?: string | null | undefined;
+      username: string;
+      access_token: string;
+      refresh_token: string;
     };
   }
 }
